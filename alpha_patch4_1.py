e551148fb00e5e19022f0aadb5a8f4ab028214ad
ROHAN MAHESH RAO <70153723+Rohanmrao@users.noreply.github.com>
2021-03-13 08:44:31 +0530

    Add files via upload
    added a feature. Buttons stop working after 3 games to prompt reset

--- alpha_patch4_1.py
+++ alpha_patch4_1.py
@@ -3,7 +3,7 @@ import tkinter
 from tkinter import * # importing all modules to make things easier, we are not short on memory anyway
 import random as ran
 
-main_window=Tk()
+main_window=Tk() # this is the variable that declares the entire window as a workable object 
 main_window.geometry("750x400")
 main_window.title("RPS machine")
 main_window.configure(bg="yellow")
@@ -16,6 +16,7 @@ cpu_points=0
 user_points=0
 a=user_points
 b=cpu_points
+final_counter=False
 
 
 def labels():
@@ -30,7 +31,7 @@ def labels():
 	cscore.grid(row=450,column=2)
 	final_winner=Label(main_window,text="",font=("Impact",16),bg="yellow")
 	final_winner.grid(row=500,column=1)
-	play_again=Label(main_window,text="",font=("Impact",15),bg="yellow") # make this grey on call
+	play_again=Label(main_window,text="",font=("Impact",15),bg="yellow") 
 	play_again.grid(row=550,column=1)
 		
 def displays():
@@ -38,6 +39,7 @@ def displays():
 	global m_display
 	global u_display
 	global winner_is
+
 
 
 	m_display=Entry(main_window,width=5) # my display is user display 
@@ -62,7 +64,7 @@ def buttons():
 	scissors_bt=Button(main_window,text="SCISSORS",width=15,height=2,pady=5,padx=5,command=click_scissors,bg="light blue",font="Impact")
 	
 	reset_bt=Button(main_window,text="Reset",command=click_reset,bg="red")
-	#contin_bt=Button(main_window,text="CONTINUE",command=click_cont,bg="red")
+
 
 	rock_bt.grid(row=30,column=0)
 	paper_bt.grid(row=30,column=1)
@@ -88,47 +90,48 @@ def click_rock():
 	global a
 	global b 
 	global final_winner
+	global final_counter
 
-
+	if final_counter==False:
 	
-	entry="rock"
-    
-	cpu_choice=data_cpu[ran.randrange(0,3,1)]
-	says_now.insert(0,cpu_choice) # updated text
-	cpu_select=1
-	if(cpu_select==1):
-		if(cpu_choice=="rock"):
-			cpu_points=cpu_points
-			user_points=user_points
-			winner_is.delete(0,END)
-			winner_is.insert(0,"Draw")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show()
-			
-			
-		elif(cpu_choice=="paper"):
-			cpu_points=cpu_points+1
-			b=cpu_points
-			u_display.delete(0,END)
-			u_display.insert(0,b) 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"CPU Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
-			
-			
-		elif(cpu_choice=="scissors"):
-			user_points=user_points+1
-			a=user_points
-			m_display.delete(0,END)
-			m_display.insert(0,a) # updated text 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"You Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
+		entry="rock"
+	    
+		cpu_choice=data_cpu[ran.randrange(0,3,1)] # randrange (0,3,1) will pick either 0 1 or 2
+		says_now.insert(0,cpu_choice) # updated text
+		cpu_select=1
+		if(cpu_select==1):
+			if(cpu_choice=="rock"):
+				cpu_points=cpu_points
+				user_points=user_points
+				winner_is.delete(0,END)
+				winner_is.insert(0,"Draw")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show()
+				
+				
+			elif(cpu_choice=="paper"):
+				cpu_points=cpu_points+1
+				b=cpu_points
+				u_display.delete(0,END)
+				u_display.insert(0,b) 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"CPU Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
+				
+				
+			elif(cpu_choice=="scissors"):
+				user_points=user_points+1
+				a=user_points
+				m_display.delete(0,END)
+				m_display.insert(0,a) # updated text 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"You Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
 			
 			
 	
@@ -148,47 +151,48 @@ def click_paper():
 	global a
 	global b 
 	global final_winner
+	global final_counter
 
+	if final_counter==False:
 	
-	
-	entry="paper"
-    
-	cpu_choice=data_cpu[ran.randrange(0,3,1)]
-	says_now.insert(0,cpu_choice) # updated text
-	cpu_select=1
-	if(cpu_select==1):
-		if(cpu_choice=="paper"):
-			cpu_points=cpu_points
-			user_points=user_points
-			winner_is.delete(0,END)
-			winner_is.insert(0,"Draw")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
-			
-			
-		elif(cpu_choice=="scissors"):
-			cpu_points=cpu_points+1
-			b=cpu_points
-			u_display.delete(0,END)
-			u_display.insert(0,b) 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"CPU Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
-			
-			
-		elif(cpu_choice=="rock"):
-			user_points=user_points+1
-			a=user_points
-			m_display.delete(0,END)
-			m_display.insert(0,a) # updated text 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"You Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show()
+		entry="paper"
+	    
+		cpu_choice=data_cpu[ran.randrange(0,3,1)]
+		says_now.insert(0,cpu_choice) # updated text
+		cpu_select=1
+		if(cpu_select==1):
+			if(cpu_choice=="paper"):
+				cpu_points=cpu_points
+				user_points=user_points
+				winner_is.delete(0,END)
+				winner_is.insert(0,"Draw")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
+				
+				
+			elif(cpu_choice=="scissors"):
+				cpu_points=cpu_points+1
+				b=cpu_points
+				u_display.delete(0,END)
+				u_display.insert(0,b) 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"CPU Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
+				
+				
+			elif(cpu_choice=="rock"):
+				user_points=user_points+1
+				a=user_points
+				m_display.delete(0,END)
+				m_display.insert(0,a) # updated text 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"You Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show()
 			
 				
 		
@@ -208,45 +212,48 @@ def click_scissors():
 	global a
 	global b 
 	global final_winner
+	global final_counter
+
+	if final_counter==False:
 
 
 	
-	entry="scissors"
-    
-	cpu_choice=data_cpu[ran.randrange(0,3,1)]
-	says_now.insert(0,cpu_choice) # updated text
-	cpu_select=1
-	if(cpu_select==1):
-		if(cpu_choice=="scissors"):
-			cpu_points=cpu_points
-			user_points=user_points
-			winner_is.delete(0,END)
-			winner_is.insert(0,"Draw")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
-			
-		elif(cpu_choice=="rock"):
-			cpu_points=cpu_points+1
-			b=cpu_points
-			u_display.delete(0,END)
-			u_display.insert(0,b) 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"CPU Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
-			
-		elif(cpu_choice=="paper"):
-			user_points=user_points+1
-			a=user_points
-			m_display.delete(0,END)
-			m_display.insert(0,a) # updated text 
-			winner_is.delete(0,END)
-			winner_is.insert(0,"You Won!")
-			says_now.delete(0,END)
-			says_now.insert(0,cpu_choice)
-			final_show() 
+		entry="scissors"
+	    
+		cpu_choice=data_cpu[ran.randrange(0,3,1)]
+		says_now.insert(0,cpu_choice) # updated text
+		cpu_select=1
+		if(cpu_select==1):
+			if(cpu_choice=="scissors"):
+				cpu_points=cpu_points
+				user_points=user_points
+				winner_is.delete(0,END)
+				winner_is.insert(0,"Draw")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
+				
+			elif(cpu_choice=="rock"):
+				cpu_points=cpu_points+1
+				b=cpu_points
+				u_display.delete(0,END)
+				u_display.insert(0,b) 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"CPU Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
+				
+			elif(cpu_choice=="paper"):
+				user_points=user_points+1
+				a=user_points
+				m_display.delete(0,END)
+				m_display.insert(0,a) # updated text 
+				winner_is.delete(0,END)
+				winner_is.insert(0,"You Won!")
+				says_now.delete(0,END)
+				says_now.insert(0,cpu_choice)
+				final_show() 
 			
 
 
@@ -266,6 +273,7 @@ def click_reset():
 	global b
 
 	global play_again
+	global final_counter
 
 	
 
@@ -284,6 +292,8 @@ def click_reset():
 	m_display.insert(0,"0") # default text 
 	a=0
 	b=0
+	final_counter=False
+
 
 	final_winner=Label(main_window,text="                                              ",font=("Impact",16),bg="yellow")
 	final_winner.grid(row=500,column=1)
@@ -295,6 +305,8 @@ def final_show():
 	global a 
 	global b
 	global final_winner
+	global final_counter
+
 
 	 
 
@@ -303,13 +315,17 @@ def final_show():
 		final_winner.grid(row=500,column=1)
 		play_again=Label(main_window,text="Hit Reset !",font=("Impact",15),bg="yellow") 
 		play_again.grid(row=550,column=1)
+		final_counter=True
+		
 		
 	elif(b>=3):
 		final_winner=Label(main_window,text="CPU Won the Game !",font=("Impact",16),bg="light blue")
 		final_winner.grid(row=500,column=1)
 		play_again=Label(main_window,text="Hit Reset !",font=("Impact",15),bg="yellow") 
 		play_again.grid(row=550,column=1)
+		final_counter=False
 		
+
 labels()
 displays()
 buttons()
